9592689e4f74682e33a8c6cdf601e328354e811a
Gowtham Kumar Rodda <gowtham@olivetech.net>
2019-07-02 15:56:59 +0530

    added ci- concourse files

---
+++ ci/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eu
+
+export fly_target=${fly_target:-hello-world}
+
+fly -t ${fly_target} validate-pipeline --config pipeline.yml
+fly -t ${fly_target} set-pipeline -p hello-world-pipeline -c pipeline.yml --load-vars-from=credentials.yml
+fly -t ${fly_target} unpause-pipeline -p hello-world-pipeline
+fly -t ${fly_target} trigger-job -w -j hello-world-pipeline/job-unit-test-master
