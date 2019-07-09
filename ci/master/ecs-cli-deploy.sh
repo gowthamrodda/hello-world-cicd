58570400bdfd396ad80218a0ad4ea29817f07f80
Gowtham Kumar Rodda <gowtham@olivetech.net>
2019-07-09 16:40:22 +0530

    adds ecs-cli-deploy and compose file

---
+++ ci/master/ecs-cli-deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+COMPOSE_FILE="hello-world-cicd/ci/master/ecs-master.compose.yml"
+PROJECT_PREFIX="hello-world"
+DEPLOY_TO="master"
+CLUSTER="Alpha"
+AWS_ACCESS_KEY_ID=$1
+AWS_SECRET_ACCESS_KEY=$2
+REGION="ap-south-1"
+
+/ecs-cli configure \
+          profile --profile-name storage-profile \
+          --access-key ${AWS_ACCESS_KEY_ID} \
+          --secret-key ${AWS_SECRET_ACCESS_KEY}
+
+/ecs-cli compose \
+           --verbose \
+           --file ${COMPOSE_FILE} \
+           --project-name ${PROJECT_PREFIX}-${DEPLOY_TO} \
+           --cluster ${CLUSTER} \
+           --region ${REGION} \
+           create
+
+/ecs-cli compose \
+           --verbose \
+           --file ${COMPOSE_FILE} \
+           --project-name ${PROJECT_PREFIX}-${DEPLOY_TO} \
+           --cluster ${CLUSTER} --region ${REGION} service up  --timeout 10
+
--- ci/run.sh
+++ ci/run.sh
@@ -8,3 +8,5 @@ fly -t ${fly_target} validate-pipeline --config pipeline.yml
 fly -t ${fly_target} set-pipeline -p hello-world-pipeline -c pipeline.yml --load-vars-from=credentials.yml
 fly -t ${fly_target} unpause-pipeline -p hello-world-pipeline
 fly -t ${fly_target} trigger-job -w -j hello-world-pipeline/job-unit-test-master
+
+# fly -t hello-world destroy-pipeline -p hello-world-pipeline
