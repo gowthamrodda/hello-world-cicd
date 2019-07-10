e50b9ff22ec9336da872fbe8707e2dd1606f8ecc
Gowtham Kumar Rodda <gowtham@olivetech.net>
2019-07-10 15:37:50 +0530

    adds ecs params file

--- ci/master/ecs-cli-deploy.sh
+++ ci/master/ecs-cli-deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 COMPOSE_FILE="hello-world-cicd/ci/master/ecs-master.compose.yml"
-PROJECT_PREFIX="hello-world"
+PARAMS_FILE="hello-world-cicd/ci/master/ecs-params.yml"
+PROJECT_NAME="alpha"
 DEPLOY_TO="master"
 CLUSTER="alpha"
 AWS_ACCESS_KEY_ID=$1
@@ -9,21 +10,24 @@ AWS_SECRET_ACCESS_KEY=$2
 REGION="ap-south-1"
 
 /ecs-cli configure \
-          profile --profile-name storage-profile \
+          profile --profile-name softdev \
           --access-key ${AWS_ACCESS_KEY_ID} \
           --secret-key ${AWS_SECRET_ACCESS_KEY}
 
 /ecs-cli compose \
            --verbose \
            --file ${COMPOSE_FILE} \
-           --project-name ${PROJECT_PREFIX}-${DEPLOY_TO} \
-           --cluster ${CLUSTER} \
-           --region ${REGION} \
-           create
+           --ecs-params ${PARAMS_FILE} \
+           --project-name ${PROJECT_NAME} \
+            service up \
+           --create-log-groups \
+           --cluster-config ${CLUSTER}-config
+
 
 /ecs-cli compose \
            --verbose \
-           --file ${COMPOSE_FILE} \
-           --project-name ${PROJECT_PREFIX}-${DEPLOY_TO} \
-           --cluster ${CLUSTER} --region ${REGION} service up  --timeout 10
+           --project-name ${PROJECT_NAME} \
+            service ps \
+           --create-log-groups \
+           --cluster-config ${CLUSTER}-config
 
